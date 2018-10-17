8f4aa9580d2135853be058083afa52558f9181e2
Aline Freitas <aline.freitas@escale.com.br>
2018-10-17 10:07:21 -0300

    Add IndependentScreens

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.IndependentScreens
 import XMonad.Util.Cursor
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
@@ -10,6 +12,7 @@ import XMonad.Util.SpawnOnce
 import DBus
 import DBus.Client
 import System.IO
+import qualified XMonad.StackSet as W
 
 spotifyCtrl :: Client -> MemberName -> X ()
 spotifyCtrl client command = liftIO $ do
@@ -24,22 +27,35 @@ myManageHook = composeAll
     , className =? "trayer" --> doIgnore
     ]
 
+myWorkspaces = withScreens 2 ["Term", "Web", "Files"]
+-- myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
 myStatusBar = "xmobar"
 
 myStartupHook = do
   setDefaultCursor xC_left_ptr
+  setWMName "LG3D"
+  spawn "~/bin/check-hdmi.hs"
   spawn "trayer --edge top --align right --SetDockType true --expand true --width 10 --transparent true --tint 0x191970 --height 12 --SetPartialStrut true"
-  spawn "xsetroot -solid midnightblue"
+  spawn "xloadimage -onroot -fullscreen ~/Images/xmonad.jpg"
   spawn "dhcpcd-gtk"
   spawn "parcellite"
-  spawn "xscreensaver -no-splash -lock"
+  spawn "blueman-applet"
+  spawn "start-pulseaudio-x11"
+  spawn "xrandr --output HDMI-1 --mode 1920x1080 --output eDP-1 --mode 1366x768 --rate 60.11 --right-of HDMI-1"
+  spawn "xscreensaver -no-splash"
+  spawn "slack"
 
 
 myKeys = [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
          , ((mod4Mask .|. shiftMask, xK_k), spawn "onboard")
+         , ((mod4Mask, xK_p), spawn "dmenu_run")
          , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
          , ((0, xK_Print), spawn "scrot")
-         ]
+         ]++
+         [((m .|. mod4Mask, k), windows $ onCurrentScreen f i)
+            | (i, k) <- zip (workspaces' def) [xK_1 .. xK_9]
+            , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 myKeysP dbusClient = [ ("<XF86AudioMute>", spawn "amixer -q sset Master toggle")
                      , ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 5+ unmute")
@@ -47,7 +63,12 @@ myKeysP dbusClient = [ ("<XF86AudioMute>", spawn "amixer -q sset Master toggle")
                      , ("<XF86AudioPlay>", spotifyCtrl dbusClient "PlayPause")
                      , ("<XF86AudioNext>", spotifyCtrl dbusClient "Next")
                      , ("<XF86AudioPrev>", spotifyCtrl dbusClient "Previous")
-                     ]
+                     ]++
+                    [ (otherModMasks ++ "M-" ++ [key], action tag)
+                      | (tag, key)  <- zip myWorkspaces "123456789"
+                      , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
+                                                          , ("S-", windows . W.shift)]
+                    ]
 
 main = do
     xmproc <- spawnPipe myStatusBar
@@ -56,6 +77,7 @@ main = do
         { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
                         <+> manageHook def
         , layoutHook = avoidStruts  $  layoutHook def
+        , workspaces = myWorkspaces
         , handleEventHook = handleEventHook def <+> docksEventHook
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
@@ -65,4 +87,4 @@ main = do
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         , startupHook = myStartupHook
         } `additionalKeys` myKeys
-          `additionalKeysP` myKeysP (client)
+          `additionalKeysP` myKeysP client
