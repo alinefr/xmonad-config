48cf868133b87d766357e23cbe1d88981c14bca9
Aline Freitas <aline@alinefreitas.com.br>
2018-12-15 12:42:50 -0200

    Remove slack as its not needed at home

--- xmonad.hs
+++ xmonad.hs
@@ -45,7 +45,6 @@ myStartupHook = do
   spawn "start-pulseaudio-x11"
   spawn "xrandr --output HDMI-1 --mode 1920x1080 --output eDP-1 --mode 1366x768 --rate 60.11 --right-of HDMI-1"
   spawn "xscreensaver -no-splash"
-  spawn "slack"
 
 myXPConfig = defaultXPConfig { font = "xft:inconsolata:size=11:antialias=true:hinting=true:hintstyle=hintfull"
                              , bgColor = "#171717"
